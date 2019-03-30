5ff9ee2bbbfbd46eae7c6b7ef96b5fdc589cee6f
王俊杰 <galileo233@163.com>
2019-03-30 22:04:19 +0800

    修正拼写错误

--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -9,7 +9,7 @@ import edu.princeton.cs.algs4.StdRandom;
 public class QuickSort extends ProtoSort{
 
     /**划分**/
-    private static int partion(Comparable[] a, int low, int high){
+    private static int partition(Comparable[] a, int low, int high){
         //high+1是由于要使用--j，而在循环做iteration时--j的效率≥j--
         int i = low, j = high+1;
         while (true){
@@ -34,7 +34,7 @@ public class QuickSort extends ProtoSort{
             //交换i，j，把大的放右边，小的放左边
             exchange(a, i, j);
         }
-        //把a[low]放到partion左部分的最右边元素位置去，即j，此时a[low]左边≤a[low]， a[low]右边都≥a[low]
+        //把a[low]放到partition左部分的最右边元素位置去，即j，此时a[low]左边≤a[low]， a[low]右边都≥a[low]
         exchange(a, j, low);
         return j;
     }
@@ -44,7 +44,7 @@ public class QuickSort extends ProtoSort{
         if (low >= high){
             return;
         }
-        int j = partion(a, low, high);
+        int j = partition(a, low, high);
         sort(a, low, j-1);
         sort(a, j+1, high);
     }
