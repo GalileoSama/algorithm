3774bda055aab3ddf37a61fc9a8600679d6283a1
王俊杰 <galileo233@163.com>
2019-06-12 21:25:18 +0800

    二叉查询树 测试完毕

--- src/search/BST.java
+++ src/search/BST.java
@@ -1,5 +1,9 @@
 package search;
 
+import edu.princeton.cs.algs4.Queue;
+
+import java.util.Random;
+
 /**
  * @author galileo
  * @date 2019/6/10 15:59
@@ -88,6 +92,7 @@ public class BST<Key extends Comparable<Key>, Value> {
                 node = node.left;
             }else {
                 node.value =  value;
+                return;
             }
         }
 
@@ -162,6 +167,18 @@ public class BST<Key extends Comparable<Key>, Value> {
         return min(node.left);
     }
 
+    /** 递归max **/
+    public Key max(){
+        return max(root).key;
+    }
+
+    private Node max(Node node){
+        if (node.right == null){
+            return node;
+        }
+        return max(node.right);
+    }
+
     /** 递归select **/
     public Key select(int k){
         return select(root, k).key;
@@ -183,7 +200,6 @@ public class BST<Key extends Comparable<Key>, Value> {
         }
 
     }
-
 
     /** 递归rank **/
     public int rank(Key key){
@@ -218,4 +234,98 @@ public class BST<Key extends Comparable<Key>, Value> {
         return node;
     }
 
+    /** 删除 **/
+    public void delete(Key key){
+        root = delete(root, key);
+    }
+
+    private Node delete(Node node, Key key){
+        if (node == null){
+            return null;
+        }
+        //寻找要删除的节点（键为key）
+        int compare = key.compareTo(node.key);
+        if (compare < 0){
+            node.left = delete(node.left, key);
+        }else if (compare > 0){
+            node.right = delete(node.right, key);
+        }else {
+            //若无右子树，把左子树给父节点（同deleteMin）
+            if (node.right == null){
+                return node.left;
+            }
+            //若无左子树，把右子树给父节点
+            if (node.left == null){
+                return node.right;
+            }
+            //见算法4th P260 ：四个步骤
+            Node t = node;
+            node = min(t.right);
+            node.right = deleteMin(node.right);
+            node.left = t.left;
+        }
+        node.n = 1 + size(node.left) + size(node.right);
+        return node;
+    }
+
+    /** 范围查询 **/
+    public Iterable<Key> keys(){
+        return keys(min(), max());
+    }
+
+    public Iterable<Key> keys(Key lo, Key hi){
+        Queue<Key> queue = new Queue<>();
+        keys(root, queue, lo, hi);
+        return queue;
+    }
+
+    private void keys(Node node, Queue<Key> queue, Key lo, Key hi){
+        if (node == null){
+            return;
+        }
+        int compareLo = lo.compareTo(node.key);
+        int compareHi = hi.compareTo(node.key);
+        if (compareLo < 0){
+            keys(node.left, queue, lo, hi);
+        }
+        if (compareLo <= 0 && compareHi >= 0){
+            queue.enqueue(node.key);
+        }
+        if (compareHi > 0){
+            keys(node.right, queue, lo, hi);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("=====start=====");
+        Random random = new Random();
+        BST<Integer, Integer> bst = new BST<>();
+        System.out.println("======测试递归put======");
+
+        for (int i = 0; i<10;i++){
+            int k = random.nextInt(30);
+            bst.putByRecursion(k, i);
+        }
+        //用于测试delete和rank用
+        bst.putByRecursion(13, 123);
+        Iterable<Integer> keys = bst.keys();
+        keys.forEach(k ->System.out.println("keys : " + k ));
+
+        System.out.println("======测试floor======");
+        System.out.println(bst.floor(11));
+        System.out.println("======测试rank======");
+        System.out.println(bst.rank(13));
+        System.out.println("======测试min======");
+        System.out.println(bst.min());
+        System.out.println("======测试max======");
+        System.out.println(bst.max());
+        System.out.println("======测试deleteMin======");
+        bst.deleteMin();
+        System.out.println(bst.min());
+        System.out.println("======测试delete======");
+        bst.delete(13);
+        Iterable<Integer> keys1 = bst.keys();
+        keys1.forEach(k ->System.out.println("keys : " + k ));
+        System.out.println("======end======");
+    }
 }
