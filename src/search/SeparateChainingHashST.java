5a62f31636f46871e413c63bac8880d7e8805d1f
王俊杰 <galileo233@163.com>
2019-06-18 15:36:13 +0800

    拉链式散列表实现

---
+++ src/search/SeparateChainingHashST.java
@@ -0,0 +1,38 @@
+package search;
+
+/**
+ * @author galileo
+ * @date 2019/6/18 15:27
+ */
+public class SeparateChainingHashST<Key extends Comparable<Key>, Value> {
+    /** 数组长度 **/
+    private int m;
+    /** 链表数量 **/
+    private int n;
+    /** 链表数组 **/
+    private SequentialSearchST<Key, Value>[] st;
+
+    public SeparateChainingHashST() {
+        this(997);
+    }
+
+    public SeparateChainingHashST(int m) {
+        this.m = m;
+        st = (SequentialSearchST<Key, Value>[]) new SequentialSearchST[m];
+        for (int i = 0; i < m; i++){
+            st[i] = new SequentialSearchST<>();
+        }
+    }
+
+    private int hash(Key key){
+        return (key.hashCode() & 0x7fffffff) % m;
+    }
+
+    public Value get(Key key){
+        return st[hash(key)].get(key);
+    }
+
+    public void put(Key key, Value value){
+        st[hash(key)].put(key, value);
+    }
+}
