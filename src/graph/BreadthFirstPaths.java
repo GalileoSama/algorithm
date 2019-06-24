0e142a53bc52e3b6a47b8ff4e656406396b26362
王俊杰 <galileo233@163.com>
2019-06-24 16:36:45 +0800

    添加获取最短路径的距离

--- src/graph/BreadthFirstPaths.java
+++ src/graph/BreadthFirstPaths.java
@@ -10,17 +10,23 @@ import edu.princeton.cs.algs4.Stack;
 public class BreadthFirstPaths {
     private boolean[] marked;
     private int[] edgeTo;
+    private int[] distTo;
     private int s;
 
     public BreadthFirstPaths(Graph graph, int s) {
         marked = new boolean[graph.V()];
         edgeTo = new int[graph.V()];
+        distTo = new int[graph.V()];
+        for (int v = 0; v < graph.V(); v++) {
+            distTo[v] = Integer.MAX_VALUE;
+        }
         this.s = s;
         bfs(graph, s);
     }
 
     public void bfs(Graph graph, int s){
         Queue<Integer> queue = new Queue<>();
+        distTo[s] = 0;
         marked[s] = true;
         queue.enqueue(s);
         while (!queue.isEmpty()){
@@ -28,6 +34,7 @@ public class BreadthFirstPaths {
             for (int w : graph.adj(v)){
                 if (!hasPathTo(w)){
                     marked[w] = true;
+                    distTo[w] = distTo[v] + 1;
                     queue.enqueue(w);
                     edgeTo[w] = v;
                 }
@@ -45,6 +52,14 @@ public class BreadthFirstPaths {
         }
         path.push(s);
         return path;
+    }
+
+    /**距离**/
+    public int distTo(int v){
+        if (!hasPathTo(v)){
+            return 0;
+        }
+        return distTo[v];
     }
 
     public boolean hasPathTo(int w){
