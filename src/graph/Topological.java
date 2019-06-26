dde0a6b1cad57359f934e9ccb8d39f9738abcbf1
王俊杰 <galileo233@163.com>
2019-06-26 16:23:58 +0800

    有向图 拓扑排序

---
+++ src/graph/Topological.java
@@ -0,0 +1,28 @@
+package graph;
+
+/**
+ * @author galileo
+ * @date 2019/6/26 16:19
+ */
+public class Topological {
+    /** 顶点拓扑顺序 **/
+    private Iterable<Integer> order;
+
+    public Topological(Digraph digraph) {
+        //检测是否是有向无环图
+        DirectedCycle directedCycle = new DirectedCycle(digraph);
+        if (!directedCycle.hasCycle()){
+            DepthFirstOrder depthFirstOrder = new DepthFirstOrder(digraph);
+            order = depthFirstOrder.getReversePost();
+        }
+    }
+
+    public Iterable<Integer> getOrder() {
+        return order;
+    }
+
+    /**是否是有向无环图**/
+    public boolean isDAG(){
+        return order != null;
+    }
+}
