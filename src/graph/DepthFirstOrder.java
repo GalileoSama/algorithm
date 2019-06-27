a292721584b3f3a6af53290cd41990397e63cfbb
王俊杰 <galileo233@163.com>
2019-06-27 20:48:43 +0800

    测试排序

--- src/graph/DepthFirstOrder.java
+++ src/graph/DepthFirstOrder.java
@@ -50,4 +50,44 @@ public class DepthFirstOrder {
     public Stack<Integer> getReversePost() {
         return reversePost;
     }
+
+    public static void main(String[] args) {
+        Digraph digraph = new Digraph(13);
+        digraph.addEdge(4,2);
+        digraph.addEdge(2,3);
+        digraph.addEdge(3,2);
+        digraph.addEdge(6,0);
+        digraph.addEdge(0,1);
+        digraph.addEdge(2,0);
+        digraph.addEdge(11,12);
+        digraph.addEdge(12,9);
+        digraph.addEdge(9,10);
+        digraph.addEdge(9,11);
+        digraph.addEdge(8,9);
+        digraph.addEdge(10,12);
+        digraph.addEdge(11,4);
+        digraph.addEdge(4,3);
+        digraph.addEdge(3,5);
+        digraph.addEdge(7,8);
+        digraph.addEdge(8,7);
+        digraph.addEdge(5,4);
+        digraph.addEdge(0,5);
+        digraph.addEdge(6,4);
+        digraph.addEdge(6,9);
+        digraph.addEdge(7,6);
+
+        DepthFirstOrder depthFirstOrder = new DepthFirstOrder(digraph);
+        for (int i : depthFirstOrder.getPost()){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i : depthFirstOrder.getReversePost()){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        DepthFirstOrder depthFirstOrder1 = new DepthFirstOrder(digraph.reverse());
+        for (int i : depthFirstOrder1.getReversePost()){
+            System.out.print(i + " ");
+        }
+    }
 }
