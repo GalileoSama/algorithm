f97f30025f42f75151de471e5f1c8624fe467743
王俊杰 <galileo233@163.com>
2019-07-01 09:58:39 +0800

    延迟实现的prime算法

---
+++ src/graph/weight/LazyPrimeMst.java
@@ -0,0 +1,53 @@
+package graph.weight;
+
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Queue;
+
+/**
+ * @author galileo
+ * @date 2019/7/1 9:46
+ */
+public class LazyPrimeMst {
+    private boolean[] marked;
+    private Queue<Edge> mst;
+    private MinPQ<Edge> pq;
+
+    public LazyPrimeMst(EdgeWeightedGraph graph) {
+        marked = new boolean[graph.getV()];
+        mst = new Queue<>();
+        pq = new MinPQ<>();
+
+        visit(graph, 0);
+        while (!pq.isEmpty()){
+            Edge edge = pq.delMin();
+            int v = edge.either();
+            int w = edge.other(v);
+            if (marked[v] && marked[w]) {
+                continue;
+            }
+            //添加到最小生成树种
+            mst.enqueue(edge);
+            if (!marked[v]){
+                visit(graph, v);
+            }
+            if (!marked[w]){
+                visit(graph, w);
+            }
+        }
+    }
+
+    private void visit(EdgeWeightedGraph graph, int v){
+        marked[v] = true;
+
+        for (Edge edge : graph.adj(v)){
+            //边未失效
+            if (!marked[edge.other(v)]){
+                pq.insert(edge);
+            }
+        }
+    }
+
+    public Iterable<Edge> edges(){
+        return mst;
+    }
+}
