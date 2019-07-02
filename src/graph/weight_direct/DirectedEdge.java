dc08ca72788a6a1cc75b7bc156e32b95aa8f517c
王俊杰 <galileo233@163.com>
2019-07-02 11:16:13 +0800

    有向边

---
+++ src/graph/weight_direct/DirectedEdge.java
@@ -0,0 +1,34 @@
+package graph.weight_direct;
+
+/**
+ * @author galileo
+ * @date 2019/7/2 11:12
+ */
+public class DirectedEdge {
+    private int v;
+    private int w;
+    private double weight;
+
+    public DirectedEdge(int v, int w, double weight) {
+        this.v = v;
+        this.w = w;
+        this.weight = weight;
+    }
+
+    public int from() {
+        return v;
+    }
+
+    public int to() {
+        return w;
+    }
+
+    public double weight() {
+        return weight;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d-%d %.2f",v,w,weight);
+    }
+}
