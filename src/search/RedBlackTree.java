4b31d83d2cb17c32fa898c76a3d12f650cd28f93
galileo <galileo233@163.com>
2020-07-25 21:54:11 +0800

    红黑树增加注释

--- src/search/RedBlackTree.java
+++ src/search/RedBlackTree.java
@@ -1,217 +1,244 @@
 package search;
 
-import edu.princeton.cs.algs4.Queue;
-
 import java.util.Random;
+
+import edu.princeton.cs.algs4.Queue;
 
 /**
  * @author galileo
  * @date 2019/6/13 20:40
  */
 public class RedBlackTree<Key extends Comparable<Key>, Value> {
-    private final static  boolean RED = true;
-    private final static  boolean BLACK = false;
-    private Node root;
 
-    class Node{
-        Key key;
-        Value value;
-        Node left, right;
-        boolean color;
-        int n;
+	private final static boolean RED = true;
 
-        public Node(Key key, Value value, boolean color, int n) {
-            this.key = key;
-            this.value = value;
-            this.color = color;
-            this.n = n;
-        }
-    }
+	private final static boolean BLACK = false;
 
-    /** 节点数 **/
-    public int size(){
-        return size(root);
-    }
+	private Node root;
 
-    private int size(Node x){
-        if (x == null){
-            return 0;
-        }
-        return x.n;
-    }
+	class Node {
 
-    /** 判断颜色是否为红色 **/
-    private boolean isRed(Node node){
-        if (node == null){
-            return false;
-        }
-        return RED == node.color;
-    }
+		Key key;
 
-    /** 右旋转 **/
-    public Node rotateRight(Node h){
-        assert isRed(h.left);
-        //h的右节点 比h大 继承h的一切成为新的根
-        Node x = h.left;
+		Value value;
 
-        h.left = x.right;
-        x.right = h;
+		Node left, right;
 
-        x.color = h.color;
-        h.color = RED;
+		boolean color;
 
-        x.n = h.n;
-        h.n = 1 + size(h.right) + size(h.left);
-        return x;
-    }
+		int n;
 
-    /** 左旋转 **/
-    public Node rotateLeft(Node h){
-        assert isRed(h.right);
-        //h的右节点 比h大 继承h的一切成为新的根
-        Node x = h.right;
+		public Node(Key key, Value value, boolean color, int n) {
+			this.key = key;
+			this.value = value;
+			this.color = color;
+			this.n = n;
+		}
+	}
 
-        h.right = x.left;
-        x.left = h;
+	/**
+	 * 节点数
+	 **/
+	public int size() {
+		return size(root);
+	}
 
-        x.color = h.color;
-        h.color = RED;
+	private int size(Node x) {
+		if (x == null) {
+			return 0;
+		}
+		return x.n;
+	}
 
-        x.n = h.n;
-        h.n = 1 + size(h.left) + size(h.right);
-        return x;
-    }
+	/**
+	 * 判断颜色是否为红色
+	 **/
+	private boolean isRed(Node node) {
+		if (node == null) {
+			return false;
+		}
+		return RED == node.color;
+	}
 
-    public void flipColor(Node h){
-        h.color = RED;
-        h.left.color = BLACK;
-        h.right.color = BLACK;
-    }
+	/**
+	 * 右旋转
+	 **/
+	public Node rotateRight(Node h) {
+		assert isRed(h.left);
+		//h的右节点 比h大 继承h的一切成为新的根
+		Node x = h.left;
 
-    /** 递归get **/
-    public Value get(Key key){
-        return get(root, key);
-    }
+		h.left = x.right;
+		x.right = h;
 
-    private Value get(Node node, Key key){
-        if (node == null){
-            return null;
-        }
-        int compare = node.key.compareTo(key);
-        if (compare < 0){
-            return get(node.right, key);
-        }else if (compare > 0){
-            return get(node.left, key);
-        }else {
-            return node.value;
-        }
-    }
+		x.color = h.color;
+		h.color = RED;
 
-    /** 插入 **/
-    public void put(Key key, Value value){
-        root = put(root, key, value);
-        root.color = BLACK;
-    }
+		x.n = h.n;
+		h.n = 1 + size(h.right) + size(h.left);
+		return x;
+	}
 
-    private Node put(Node h, Key key, Value value){
-        if (h == null){
-            return new Node(key, value, RED, 1);
-        }
+	/**
+	 * 左旋转
+	 **/
+	public Node rotateLeft(Node h) {
+		assert isRed(h.right);
+		//h的右节点 比h大 继承h的一切成为新的根
+		Node x = h.right;
 
-        int compare = key.compareTo(h.key);
-        if (compare < 0){
-            h.left = put(h.left, key, value);
-        }else if (compare > 0){
-            h.right = put(h.right, key, value);
-        }else {
-            h.value = value;
-        }
+		h.right = x.left;
+		x.left = h;
 
-        if (!isRed(h.left) && isRed(h.right) ){
-            h = rotateLeft(h);
-        }
-        if (isRed(h.left) && isRed(h.left.left)){
-            h = rotateRight(h);
-        }
-        if (isRed(h.left) && isRed(h.right)){
-            flipColor(h);
-        }
+		x.color = h.color;
+		h.color = RED;
 
-        h.n = 1 + size(h.left) + size(h.right);
-        return h;
-    }
+		x.n = h.n;
+		h.n = 1 + size(h.left) + size(h.right);
+		return x;
+	}
 
-    /** 范围查询 **/
-    public Iterable<Key> keys(){
-        return keys(min(), max());
-    }
+	public void flipColor(Node h) {
+		h.color = RED;
+		h.left.color = BLACK;
+		h.right.color = BLACK;
+	}
 
-    public Iterable<Key> keys(Key lo, Key hi){
-        Queue<Key> queue = new Queue<>();
-        keys(root, queue, lo, hi);
-        return queue;
-    }
+	/**
+	 * 递归get
+	 **/
+	public Value get(Key key) {
+		return get(root, key);
+	}
 
-    private void keys(Node node, Queue<Key> queue, Key lo, Key hi){
-        if (node == null){
-            return;
-        }
-        int compareLo = lo.compareTo(node.key);
-        int compareHi = hi.compareTo(node.key);
-        if (compareLo < 0){
-            keys(node.left, queue, lo, hi);
-        }
-        if (compareLo <= 0 && compareHi >= 0){
-            queue.enqueue(node.key);
-        }
-        if (compareHi > 0){
-            keys(node.right, queue, lo, hi);
-        }
-    }
+	private Value get(Node node, Key key) {
+		if (node == null) {
+			return null;
+		}
+		int compare = node.key.compareTo(key);
+		if (compare < 0) {
+			return get(node.right, key);
+		} else if (compare > 0) {
+			return get(node.left, key);
+		} else {
+			return node.value;
+		}
+	}
 
-    /** 递归min **/
-    public Key min(){
-        return min(root).key;
-    }
+	/**
+	 * 插入
+	 **/
+	public void put(Key key, Value value) {
+		root = put(root, key, value);
+		root.color = BLACK;
+	}
 
-    private Node min(Node node){
-        if (node.left == null){
-            return node;
-        }
-        return min(node.left);
-    }
+	private Node put(Node h, Key key, Value value) {
+		if (h == null) {
+			return new Node(key, value, RED, 1);
+		}
 
-    /** 递归max **/
-    public Key max(){
-        return max(root).key;
-    }
+		int compare = key.compareTo(h.key);
+		if (compare < 0) {
+			h.left = put(h.left, key, value);
+		} else if (compare > 0) {
+			h.right = put(h.right, key, value);
+		} else {
+			h.value = value;
+		}
 
-    private Node max(Node node){
-        if (node.right == null){
-            return node;
-        }
-        return max(node.right);
-    }
+		//1）插入到2-node的右边，h是2-node的根
+		//2）插入到3-node的中间，h是3-node的较小节点(还会继续走下面两个if)
+		if (!isRed(h.left) && isRed(h.right))
+			h = rotateLeft(h);
+		//插入到3-node的左边，h3-node的较大值
+		if (isRed(h.left) && isRed(h.left.left))
+			h = rotateRight(h);
+		//插入到3-node的右边，h3-node的较大值
+		if (isRed(h.left) && isRed(h.right))
+			flipColor(h);
 
-    public static void main(String[] args) {
-        System.out.println("=====start=====");
-        Random random = new Random();
-        RedBlackTree<Integer, Integer> redBlackTree = new RedBlackTree<>();
-        System.out.println("======测试递归put======");
+		h.n = 1 + size(h.left) + size(h.right);
+		return h;
+	}
 
-        for (int i = 0; i<10;i++){
-            int k = random.nextInt(30);
-            redBlackTree.put(k, i);
-        }
+	/**
+	 * 范围查询
+	 **/
+	public Iterable<Key> keys() {
+		return keys(min(), max());
+	}
 
-        Iterable<Integer> keys = redBlackTree.keys();
-        keys.forEach(k ->System.out.println("keys : " + k ));
+	public Iterable<Key> keys(Key lo, Key hi) {
+		Queue<Key> queue = new Queue<>();
+		keys(root, queue, lo, hi);
+		return queue;
+	}
 
-        System.out.println("======测试min======");
-        System.out.println(redBlackTree.min());
-        System.out.println("======测试max======");
-        System.out.println(redBlackTree.max());
+	private void keys(Node node, Queue<Key> queue, Key lo, Key hi) {
+		if (node == null) {
+			return;
+		}
+		int compareLo = lo.compareTo(node.key);
+		int compareHi = hi.compareTo(node.key);
+		if (compareLo < 0) {
+			keys(node.left, queue, lo, hi);
+		}
+		if (compareLo <= 0 && compareHi >= 0) {
+			queue.enqueue(node.key);
+		}
+		if (compareHi > 0) {
+			keys(node.right, queue, lo, hi);
+		}
+	}
 
-        System.out.println("======end======");
-    }
+	/**
+	 * 递归min
+	 **/
+	public Key min() {
+		return min(root).key;
+	}
+
+	private Node min(Node node) {
+		if (node.left == null) {
+			return node;
+		}
+		return min(node.left);
+	}
+
+	/**
+	 * 递归max
+	 **/
+	public Key max() {
+		return max(root).key;
+	}
+
+	private Node max(Node node) {
+		if (node.right == null) {
+			return node;
+		}
+		return max(node.right);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("=====start=====");
+		Random random = new Random();
+		RedBlackTree<Integer, Integer> redBlackTree = new RedBlackTree<>();
+		System.out.println("======测试递归put======");
+
+		for (int i = 0; i < 10; i++) {
+			int k = random.nextInt(30);
+			redBlackTree.put(k, i);
+		}
+
+		Iterable<Integer> keys = redBlackTree.keys();
+		keys.forEach(k -> System.out.println("keys : " + k));
+
+		System.out.println("======测试min======");
+		System.out.println(redBlackTree.min());
+		System.out.println("======测试max======");
+		System.out.println(redBlackTree.max());
+
+		System.out.println("======end======");
+	}
 }
